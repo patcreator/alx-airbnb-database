eabdb1189339b93f165c9e040fe3b653d6f1ef7e
patcreator <137688649+patcreator@users.noreply.github.com>
2026-01-08 17:05:23 +0200

    Add SQL script for aggregations and window functions
    Added SQL script for aggregations and window functions, including examples for counting bookings and ranking properties.

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,32 @@
+-- =====================================
+-- Task 2: Aggregations and Window Functions
+-- =====================================
+
+-- 1️ Aggregation with COUNT and GROUP BY
+-- Find the total number of bookings made by each user
+SELECT
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    COUNT(Booking.booking_id) AS total_bookings
+FROM User
+LEFT JOIN Booking
+ON User.user_id = Booking.user_id
+GROUP BY User.user_id, User.first_name, User.last_name
+ORDER BY total_bookings DESC;
+
+
+-- 2️ Window Function (RANK)
+-- Rank properties based on the total number of bookings received
+SELECT
+    Property.property_id,
+    Property.name AS property_name,
+    COUNT(Booking.booking_id) AS total_bookings,
+    RANK() OVER (
+        ORDER BY COUNT(Booking.booking_id) DESC
+    ) AS booking_rank
+FROM Property
+LEFT JOIN Booking
+ON Property.property_id = Booking.property_id
+GROUP BY Property.property_id, Property.name
+ORDER BY booking_rank;
